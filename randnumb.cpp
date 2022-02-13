9645ded49e55b7010e5762866774fa9110c0b63f
margo.khm <kholostova@gmail.com>
2022-02-13 16:47:01 -0500

    Create randnumb.cpp
    Created task + basic structure

---
+++ randnumb.cpp
@@ -0,0 +1,25 @@
+/*Read  chapter 3 Gaddis
+
+Homework assignment 
+
+Write a math calculator program. Using the algorithm to calculate a random number found on page 130 of your textbook, calculate a random number between 1 and 300.  Calculate a second random number between  1 and the first random number.  Display the values as:
+
+   xxx
+-  yyy
+------
+wait until the  user enters an answer. Display the correct answer after the user enters their answer. 
+
+format your output nicely so that everything lines up properly. Comment your code.  Plan your program with pseudocode.  Post your .cpp and your pseudocode in Digication. Publish, then post the link here.
+
+*/
+
+#include <iostream>
+#include <cstdlib> // for rand & srand function
+using namespace std;
+
+int main {
+
+    
+}
+
+
